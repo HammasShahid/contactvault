96066850595a32036f447f6466ff5e294564a601
Hammas <hammasshahid11@gmail.com>
2026-04-14 15:23:55 +0500

    chore: initial backend setup

---
+++ backend/src/test/java/com/hammasshahid/contactvault/ContactvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hammasshahid.contactvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

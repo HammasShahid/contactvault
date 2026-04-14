547b8eb93f32c14fadce81d3bcb75259aa9b1b6a
Hammas <hammasshahid11@gmail.com>
2026-04-14 19:46:07 +0500

    feat(user): add user entity and repository

---
+++ backend/src/main/java/com/hammasshahid/contactvault/user/entity/User.java
@@ -0,0 +1,33 @@
+package com.hammasshahid.contactvault.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "created_at", insertable = false, updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+}

331e4e02e208c4b32f5e961fe7a01a345905c26c
Hammas <hammasshahid11@gmail.com>
2026-04-15 22:14:08 +0500

    feat(exception): implement validation error handlers

--- backend/src/main/java/com/hammasshahid/contactvault/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/hammasshahid/contactvault/common/exception/GlobalExceptionHandler.java
@@ -3,11 +3,15 @@ package com.hammasshahid.contactvault.common.exception;
 import com.hammasshahid.contactvault.common.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,5 +19,36 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException ex, WebRequest request) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Bad Request", ex.getMessage(), request.getDescription(false), LocalDateTime.now());
         return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationErrors(MethodArgumentNotValidException exception, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(Map.of(
+                "status", HttpStatus.BAD_REQUEST.value(),
+                "error", "Bad Request",
+                "message", "Please enter valid values",
+                "fieldErrors", errors,
+                "path", request.getDescription(false),
+                "timestamp", LocalDateTime.now()
+        ));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleTypeMismatch(MethodArgumentTypeMismatchException exception, WebRequest request) {
+        String name = exception.getName();
+        String type = exception.getRequiredType().getSimpleName();
+        Object value = exception.getValue();
+        String errorMessage = String.format(
+                "'%s' has an invalid value: '%s'. It should be a valid '%s'.",
+                name,
+                value,
+                type
+        );
+
+        return ResponseEntity.badRequest().body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Bad Request", errorMessage, request.getDescription(false), LocalDateTime.now()));
     }
 }

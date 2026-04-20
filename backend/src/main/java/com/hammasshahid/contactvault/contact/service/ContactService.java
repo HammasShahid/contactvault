723de70e8088d46ecfd9ec675ea621869221ac6c
Hammas <hammasshahid11@gmail.com>
2026-04-20 22:14:08 +0500

    feat(contact): allow adding, updating, and deleting nested emails and phones
    UPDATE -> update request contains existing phone/email id
    ADD -> update request does not contain a phone/email id
    DELETE -> phone/email record exists in database but is not present in the update request

--- backend/src/main/java/com/hammasshahid/contactvault/contact/service/ContactService.java
+++ backend/src/main/java/com/hammasshahid/contactvault/contact/service/ContactService.java
@@ -1,23 +1,27 @@
 package com.hammasshahid.contactvault.contact.service;
 
 import com.hammasshahid.contactvault.auth.service.AuthService;
+import com.hammasshahid.contactvault.common.exception.BadRequestException;
 import com.hammasshahid.contactvault.common.exception.NotFoundException;
 import com.hammasshahid.contactvault.common.exception.UnauthorizedException;
-import com.hammasshahid.contactvault.contact.dto.CreateContactRequest;
-import com.hammasshahid.contactvault.contact.dto.ContactResponse;
-import com.hammasshahid.contactvault.contact.dto.UpdateContactRequest;
+import com.hammasshahid.contactvault.contact.dto.*;
 import com.hammasshahid.contactvault.contact.entity.Contact;
 import com.hammasshahid.contactvault.contact.entity.ContactEmail;
 import com.hammasshahid.contactvault.contact.entity.ContactPhone;
 import com.hammasshahid.contactvault.contact.mapper.ContactMapper;
 import com.hammasshahid.contactvault.contact.repository.ContactRepository;
 import com.hammasshahid.contactvault.user.entity.User;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -58,7 +62,6 @@ public class ContactService {
         return contactPage.map(contactMapper::toResponse);
     }
 
-    @Transactional
     public ContactResponse update(Long id, UpdateContactRequest request) {
         Contact contact = contactRepository.findById(id).orElseThrow(() -> new NotFoundException("Contact not found."));
         if (!contact.getUser().getId().equals(authService.getCurrentUser().getId()))
@@ -67,9 +70,91 @@ public class ContactService {
         contact.setFirstName(request.getFirstName());
         contact.setLastName(request.getLastName());
 
-        // TODO: Update emails
-        // TODO: Update phones
+        syncEmails(contact, request.getEmails());
+        syncPhones(contact, request.getPhones());
 
+        contactRepository.save(contact);
         return contactMapper.toResponse(contact);
+    }
+
+    private void syncEmails(Contact contact, List<UpdateContactEmailRequest> emailRequests) {
+        /*
+         * UPDATE -> email request contains id
+         * ADD -> email request does not contain id
+         * DELETE -> email exists in database but is not present in the request
+         */
+
+        Map<Long, ContactEmail> existing =
+                contact
+                        .getEmails()
+                        .stream()
+                        .collect(Collectors.toMap(ContactEmail::getId, e -> e));
+
+        Set<ContactEmail> updated = new HashSet<>();
+
+        if (emailRequests != null) {
+            for (UpdateContactEmailRequest request : emailRequests) {
+                // UPDATE existing email
+                if (request.getId() != null) {
+
+                    if (!existing.containsKey(request.getId())) {
+                        throw new BadRequestException("Invalid email ID '" + request.getId() + "' for this contact");
+                    }
+                    ContactEmail email = existing.get(request.getId());
+                    email.setEmail(request.getEmail());
+                    email.setLabel(request.getLabel());
+                    updated.add(email);
+
+                } else {
+                    // ADD new email
+                    ContactEmail newEmail = contactMapper.toEmailEntity(request);
+                    contact.addEmail(newEmail);
+                    updated.add(newEmail);
+                }
+            }
+        }
+
+        // DELETE emails (orphanRemoval handles DB)
+        contact.getEmails().removeIf(e -> !updated.contains(e));
+    }
+
+    private void syncPhones(Contact contact, List<UpdateContactPhoneRequest> phoneRequests) {
+        /*
+         * UPDATE -> phone request contains id
+         * ADD -> phone request does not contain id
+         * DELETE -> phone exists in database but is not present in the request
+         */
+
+        Map<Long, ContactPhone> existing =
+                contact
+                        .getPhones()
+                        .stream()
+                        .collect(Collectors.toMap(ContactPhone::getId, p -> p));
+
+        Set<ContactPhone> updated = new HashSet<>();
+
+        if (phoneRequests != null) {
+            for (UpdateContactPhoneRequest request : phoneRequests) {
+                // UPDATE existing phone
+                if (request.getId() != null) {
+
+                    if (!existing.containsKey(request.getId())) {
+                        throw new BadRequestException("Invalid phone ID '" + request.getId() + "' for this contact");
+                    }
+                    ContactPhone phone = existing.get(request.getId());
+                    phone.setPhoneNumber(request.getPhoneNumber());
+                    phone.setLabel(request.getLabel());
+                    updated.add(phone);
+                } else {
+                    // ADD new phone
+                    ContactPhone newPhone = contactMapper.toPhoneEntity(request);
+                    contact.addPhone(newPhone);
+                    updated.add(newPhone);
+                }
+            }
+        }
+
+        // DELETE phones (orphanRemoval handles DB)
+        contact.getPhones().removeIf(e -> !updated.contains(e));
     }
 }

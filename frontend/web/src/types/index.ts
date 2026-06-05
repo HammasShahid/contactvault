1ac7f094d252dbc2ceb1559d9c91d46e02d75feb
Hammas <hammasshahid11@gmail.com>
2026-06-05 14:47:21 +0500

    feat(types): add contact and page types

--- frontend/web/src/types/index.ts
+++ frontend/web/src/types/index.ts
@@ -20,3 +20,34 @@ export interface UserResponse {
   lastName: string;
   email: string;
 }
+
+export interface ContactEmailResponse {
+  id: number;
+  email: string;
+  label: string;
+}
+
+export interface ContactPhoneResponse {
+  id: number;
+  phoneNumber: string;
+  label: string;
+}
+
+export interface ContactResponse {
+  id: number;
+  firstName: string;
+  lastName: string;
+  title: string;
+  emails: ContactEmailResponse[];
+  phones: ContactPhoneResponse[];
+}
+
+export interface Page<T> {
+  content: T[];
+  totalElements: number;
+  totalPages: number;
+  number: number;
+  size: number;
+  first: boolean;
+  last: boolean;
+}
